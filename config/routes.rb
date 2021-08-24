dbe03ff0847e3ef3e1cd69961c30a9a77b19f32b
yumekumo <chihachiha.ryotakun@gmail.com>
2021-08-24 07:57:49 +0000

    [Update] いいね機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    #resource :book_comments, only: [:create, :destroy]
+  end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
